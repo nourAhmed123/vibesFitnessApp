90a47b99834027a6ed3bcde551ae425cd92b0246
yasminemsalah <101893753+yasminemsalah@users.noreply.github.com>
2022-10-30 23:07:47 +0200

    Add files via upload

---
+++ lib/models/Data.dart
@@ -0,0 +1,11 @@
+class Data {
+
+  static bool isLoggedIn = false;
+
+
+
+  static final users = [
+    [1, "yasmine salah", "yasmine17@hotmail.com", "01090015134" , "images/profilee.jpg"]
+
+  ];
+}
